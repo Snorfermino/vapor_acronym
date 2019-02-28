d07bea4d31e309776135eb3041b39d60afba5f3b
Phung <phungnet001@gmail.com>
2019-02-28 22:07:35 +0700

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,27 @@
+import Vapor
+import FluentSQLite
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+
+    init(short: String, long: String) {
+        self.long = long
+        self.short = short
+    }
+}
+
+// extension Acronym: Model {
+    
+//     typealias Database = SQLiteDatabase
+//     typealias ID = Int
+
+//     public static var idKey: IDKey = \Acronym.id
+// }
+
+extension Acronym: SQLiteModel {}
+
+extension Acronym: Migration {}
+
+extension Acronym: Content {}
